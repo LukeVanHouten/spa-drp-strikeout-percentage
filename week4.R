518a0f63f75428495a443a0ea4cb2d1ec595a7f4
LukeVanHouten <lukevh@uw.edu>
2023-02-03 13:30:57 -0800

    removed spring training 2021

--- week4.R
+++ week4.R
@@ -7,10 +7,12 @@ conn <- DBI::dbConnect(RPostgres::Postgres(), dbname = "drpstatcast",
 
 dbListFields(conn, "statcast")
 
-query <- '
+query <- "
 SELECT st.pitch_type, game_date, release_speed, player_name, stand, p_throws, type, pfx_x, pfx_z, effective_speed
 FROM statcast as st
-'
+WHERE game_date
+NOT BETWEEN '2021-03-01' AND '2021-03-27'
+"
 
 sql_df <- dbGetQuery(conn, query)
 View(sql_df)
