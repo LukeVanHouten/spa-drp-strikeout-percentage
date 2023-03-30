7a7783eaf57f717a4f52a809c1052efd5205d6e3
LukeVanHouten <lukevh@uw.edu>
2023-03-30 12:19:51 -0700

    removed eda folder

---
+++ Helper Scripts/clustering.R
@@ -0,0 +1,35 @@
+library(DBI)
+library(RPostgres)
+library(tidyverse)
+library(mclust)
+
+conn <- dbConnect(Postgres(), dbname = "drpstatcast", host = "localhost",
+                  port = 5432, user = "postgres", password = "drppassword")
+
+pitch_data_query <- "
+SELECT game_year, pitcher, release_speed, pfx_x, pfx_z
+FROM statcast
+WHERE game_date NOT BETWEEN '2021-03-01' AND '2021-03-31'
+AND game_date NOT BETWEEN '2015-03-28' AND '2015-04-04'
+AND game_date NOT BETWEEN '2016-03-28' AND '2016-04-02'
+AND game_date NOT BETWEEN '2017-03-28' AND '2017-04-01'
+"
+
+pitch_data_df <- dbGetQuery(conn, pitch_data_query) %>%
+    na.omit()
+
+standardize <- function(x){
+    (x - min(x)) / diff(range(x))
+}
+
+standardized_df <- cbind(select(pitch_data_df, pitcher, game_year), 
+                         data.frame(lapply(select(pitch_data_df, -pitcher, 
+                                                  -game_year), standardize)))
+standardized_df <- head(standardized_df, 50)
+
+clustered_df <- standardized_df %>%
+    group_by(pitcher, game_year) %>%
+    summarize(clusters = n_distinct(Mclust(as.data.frame(release_speed, pfx_x, 
+                                                         pfx_z))$classification))
+
+# write.csv(clustered_df, "clustered_pitches.csv", row.names=FALSE)
---
+++ Helper Scripts/clusters_plot.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+all_pitchers <- read.csv("clustered_pitches.csv")
+
+relief_pitchers <- read.csv("relief_pitchers.csv")
+
+starting_pitchers <- setdiff(select(all_pitchers, -clusters), relief_pitchers)
+
+reliever_clusters <- reliever_clusters <- merge(all_pitchers, relief_pitchers, 
+                                                by = c("pitcher", "game_year"), 
+                                                all.x = FALSE, all.y = TRUE)
+
+starter_clusters <- anti_join(all_pitchers, reliever_clusters,
+                              by = c("pitcher", "game_year"))
+
+starter_counts <- starter_clusters %>%
+    group_by(clusters) %>%
+    summarize(num_pitchers = n_distinct(pitcher) / nrow(starter_clusters)) %>%
+    complete(clusters = 0:9, fill = list(num_pitchers = 0))
+
+reliever_counts <- reliever_clusters %>%
+    group_by(clusters) %>%
+    summarize(num_pitchers = n_distinct(pitcher) / nrow(reliever_clusters)) %>%
+    complete(clusters = 0:9, fill = list(num_pitchers = 0))
+
+counts <- rbind(starter_counts, reliever_counts)
+counts$group <- rep(c("Starters", "Relievers"), each = 10)
+
+ggplot(counts, aes(x = as.factor(clusters), y = num_pitchers, fill = group)) +
+    geom_bar(stat = "identity", position = position_dodge()) +
+    geom_line(aes(group = group, linetype = group), position = position_dodge(width = 0.9)) +
+    scale_fill_manual(values = c("green3", "red"), name = "") +
+    scale_linetype_manual(values = c("dashed", "solid"), name = "") +
+    xlab("Clusters") +
+    ylab("Percentage of Pitchers per Cluster") +
+    ggtitle("Comparison of Clusters between Starters and Relievers") +
+    theme(plot.title = element_text(hjust = 0.5))
